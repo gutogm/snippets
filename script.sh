118ff661b91672631dfce0282fe6a635891841f2
Luiz Augusto Gama de Moraes <augustomoraes90@gmail.com>
2018-11-12 18:15:34 +0000

    script.sh edited online with Bitbucket

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 # rsync + termux
 ## instalar rsync no termux
 rsync -r -a -v -e 'ssh -p 8022' ./roms u0_a11@192.168.0.87:~/roms
+
+## ERROR with ssh + termux
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+chmod 700 ~
